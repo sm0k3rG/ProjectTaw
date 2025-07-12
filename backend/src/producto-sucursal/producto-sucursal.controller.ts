d0fef2f3c77d757deda0b3922824a125884a4eec
Braayaann <brayan_garciia@hotmail.com>
2025-07-12 00:33:43 -0400

    feat: añadir validacions de login y rol de admin  para actualizar un stock

--- backend/src/producto-sucursal/producto-sucursal.controller.ts
+++ backend/src/producto-sucursal/producto-sucursal.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Post, Put, Param } from '@nestjs/common';
+import { Body, Controller, Post, Put, Param, UseGuards } from '@nestjs/common';
 import { CreateProductoSucursalDto } from './dto/create-producto-sucursal.dto';
 import { UpdateProductoSucursalDto } from './dto/update-producto-sucursal.dto';
 import { ProductoSucursalService } from './producto-sucursal.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Rol } from '@prisma/client';
 
 @Controller('producto-sucursal')
 export class ProductoSucursalController {
@@ -13,6 +17,8 @@ export class ProductoSucursalController {
   }
 
   @Put(':productoId/:sucursalId')
+  @Roles(Rol.Admin)  // Usamos el enum Rol para definir los roles permitidos
+  @UseGuards(JwtAuthGuard, RolesGuard)  // Usamos ambos guards
   async update(
     @Param('productoId') productoId: number,
     @Param('sucursalId') sucursalId: number,
