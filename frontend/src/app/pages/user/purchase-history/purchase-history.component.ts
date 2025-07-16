03ea11fb0a495056e9c88a267a62dba2ed498d88
sm0k3rG <jhosepmarca.uta@gmail.com>
2025-07-16 15:14:07 -0400

    feat: creacion de componentes de pedido y historial de compras

---
+++ frontend/src/app/pages/user/purchase-history/purchase-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './purchase-history.component.html',
+  styleUrl: './purchase-history.component.css'
+})
+export class PurchaseHistoryComponent {
+
+}
